59bc3e212d6245565db43ecc0985d989d56c64f0
ver2point0 <jmci10@hotmail.com>
2021-06-04 09:42:05 -0700

    initial start

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,21 @@
-# Write your solution here!
+
+
+def optimal_change(item_cost, amount_paid):
+	pass
+	# item_cost is a float
+	# amount_paid is a float
+
+
+
+
+
+
+
+def print_change(amount_given):
+	pass
+	# amount_given is a float
+
+
+
+optimal_change(31.51, 50)
+optimal_change(62.13, 100)
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,23 @@
-# Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class OptimalChange(unittest.TestCase):
+	
+	def test_1(self):
+		self.assertEqual(optimal_change(), )
+
+	def test_2(self):
+		self.assertEqual(optimal_change(), )
+
+	def test_3(self):
+		self.assertEqual(optimal_change(), )
+
+	def test_4(self):
+		self.assertEqual(optimal_change(), )
+
+	def test_5(self):
+		self.assertEqual(optimal_change(), )
+
+
+if __name__ == "__main__":
+	unittest.main()
