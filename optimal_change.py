fe4dee6bd1cbb52e3bf20aafd4dab056c0f806b5
ver2point0 <jmci10@hotmail.com>
2021-06-04 11:53:02 -0700

    add comments

--- optimal_change.py
+++ optimal_change.py
@@ -1,8 +1,14 @@
 class ChangeMaker:
+
+	# __init__ method
 	def __init__(self, item_cost, amount_paid):
 		self.item_cost = item_cost
 		self.amount_paid = amount_paid
+
+		# self.change_needed computes to a negative integer
 		self.change_needed = int((amount_paid * 100) - (item_cost) * 100)
+
+		# currency dictionary
 		self.currency_dict = {
 			'$100 bill': 10000,
 			'$50 bill': 5000,
@@ -16,18 +22,25 @@ class ChangeMaker:
 			'penny': 1,
 		}
 
+	# optimal_change method
 	def optimal_change(self):
+		# resulting change variable
 		change = ""
 		
+		# more change is needed to complete payment
 		if self.change_needed < 0:
 			return f"The cost is ${self.item_cost} and the amount you paid is ${self.amount_paid}. Continue depositing ${(self.amount_paid - self.item_cost) * -1}."
+
+		# exact change has been paid
 		if self.change_needed == 0:
 			return f"You've paid with exact change."
 
+		# iterate through currency dictionary to count change and apply to {change} variable
 		for key, value in self.currency_dict.items():
 			count_change = 0
 			
 			if self.change_needed >= value:
+				# The divmod() function returns a tuple containing the quotient and the remainder when argument1 (dividend is self.change_needed) is divided by argument2 (divisor is value)
 				(biggest_number, self.change_needed) = divmod(self.change_needed, value)
 
 				for i in range(0, biggest_number):
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -3,18 +3,23 @@ from optimal_change import ChangeMaker
 
 class OptimalChangeTest(unittest.TestCase):
 	
+	"""item_cost is less than amount_paid"""
 	def test_1(self):
 		self.assertEqual(ChangeMaker(62.13, 100).optimal_change(), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, 2 pennies.")
 
+	"""item_cost is less than amount_paid"""
 	def test_2(self):
 		self.assertEqual(ChangeMaker(31.51, 50).optimal_change(), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, 4 pennies.")
 
+	"""item_cost is equal to amount_paid"""
 	def test_3(self):
 		self.assertEqual(ChangeMaker(100, 100).optimal_change(), "You've paid with exact change.")
 
+	"""item_cost is initially more than amount_paid"""
 	def test_4(self):
 		self.assertEqual(ChangeMaker(1.00, 0.40).optimal_change(), "The cost is $1.0 and the amount you paid is $0.4. Continue depositing $0.6.")
 
+	"""item_cost is less than amount_paid with pennies"""
 	def test_5(self):
 		self.assertEqual(ChangeMaker(0.01, 0.04).optimal_change(), "The optimal change for an item that costs $0.01 with an amount paid of $0.04 is 3 pennies.")
 
