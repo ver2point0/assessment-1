d169f0c7d2fce284fe1ba1d28761db7c10468086
ver2point0 <jmci10@hotmail.com>
2021-06-04 11:02:47 -0700

    make class and update spec file

--- optimal_change.py
+++ optimal_change.py
@@ -1,21 +1,21 @@
+class ChangeMaker:
+	def __init__(self):
+		pass
 
 
-def optimal_change(item_cost, amount_paid):
-	pass
+
+	def optimal_change(item_cost, amount_paid):
+		pass
 	# item_cost is a float
 	# amount_paid is a float
-
-
-
-
-
-
+	# cases
+		# item_cost 62.13 > 30.00 amount_paid
+		# item_cost 62.13 == 62.13 amount_paid
+		# item_cost 62.13 < 100.00 
+	
+	# currency to use
+	currency = [100.00,50.00,20.00,10.00,5.00,1.00,0.25,0.10,0.05,0.01]
 
 def print_change(amount_given):
 	pass
 	# amount_given is a float
-
-
-
-optimal_change(31.51, 50)
-optimal_change(62.13, 100)
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,22 +1,22 @@
 import unittest
-from optimal_change import optimal_change
+from optimal_change import ChangeMaker
 
-class OptimalChange(unittest.TestCase):
+class OptimalChangeTest(unittest.TestCase):
 	
 	def test_1(self):
-		self.assertEqual(optimal_change(), )
+		self.assertEqual(ChangeMaker(62.13, 100).optimal_change(), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, 2 pennies.")
 
 	def test_2(self):
-		self.assertEqual(optimal_change(), )
+		self.assertEqual(ChangeMaker(31.51, 50).optimal_change(), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, 4 pennies.")
 
 	def test_3(self):
-		self.assertEqual(optimal_change(), )
+		self.assertEqual(ChangeMaker(100, 100).optimal_change(), "You've paid with exact change.")
 
 	def test_4(self):
-		self.assertEqual(optimal_change(), )
+		self.assertEqual(ChangeMaker(1.00, 0.40).optimal_change(), "The cost is $1.0 and the amount you paid is $0.4. Continue depositng $0.6.")
 
 	def test_5(self):
-		self.assertEqual(optimal_change(), )
+		self.assertEqual(ChangeMaker(0.04, 0.01).optimal_change(), "The optimal change for an item that costs $0.01 with an amount paid of $0.04 is 3 pennies.")
 
 
 if __name__ == "__main__":
